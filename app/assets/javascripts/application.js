b5e3822dc88184f35bf26d3eb43974df137b8f8e
RJ <rjrobinson@gmail.com>
2015-07-20 20:27:16 -0400

    added files. and some styles.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= flash_messages
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+
+	# <% user_signed_in? ?  render 'layouts/signed_in' : render 'layouts/not_signed_in' %>
+
 end
